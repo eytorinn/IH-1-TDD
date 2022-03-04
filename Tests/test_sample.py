4a8e2913ef0b8539c336a51df815e074b086c578
eytorinn <eytoralex@gmail.com>
2022-03-04 19:15:43 +0000

    check if value % 5 == 0, excluding FizzBuzz, test and impl

--- Tests/test_sample.py
+++ Tests/test_sample.py
@@ -6,4 +6,14 @@ def test_if_function_returns():
 def test_if_function_is_FizzBuzz():
     for i in range(0,100,15):
         assert FizzBuzz.fizzBuzz(i) == 'FizzBuzz'
+
+def test_if_function_is_Buzz():
+    for i in range(0,100,5):
+        if i % 15 == 0:
+            pass
+        else:
+            assert FizzBuzz.fizzBuzz(i) == 'Buzz'
+    
+
+
     
